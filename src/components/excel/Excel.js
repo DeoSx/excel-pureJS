f938c8409d051f11c526848b26167a58180076ff
damir <deosxz123@gmail.com>
2020-05-17 15:10:41 +0600

    finish render table

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,22 +2,29 @@ import {$} from '@/core/dom';
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
   }
 
   root() {
     const $root = $.create('div', 'excel');
-    this.components.forEach((Component) => {
+    this.components = this.components.map((Component) => {
       const $el = $.create('div', Component.className);
       const component = new Component($el);
-      $el.innerHTML = component.toHTML();
+
+      if (component.name) {
+        window['c' + component.name] = component;
+      }
+
+      $el.html(component.toHTML());
       $root.append($el);
+      return component;
     });
     return $root;
   }
 
   render() {
     this.$el.append(this.root());
+    this.components.forEach((component) => component.init());
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,21 @@
 import {DomListener} from '@core/DomListenter';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+    this.name = options.name || '';
+  }
+
   // return html
   toHTML() {
     return '';
+  }
+
+  init() {
+    this.initDOMListeners();
+  }
+
+  destroy() {
+    this.removeDOMListeners();
   }
 }
